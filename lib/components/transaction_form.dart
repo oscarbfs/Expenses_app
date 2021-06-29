deaaeddd2ba0e446b1be0c7a74468cb9115c88b0
OscarBorges <oscar.borgesfs@gmail.com>
2021-06-29 16:34:15 -0300

    Correção do Formulário

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
-class TransactionForm extends StatelessWidget {
-  final titleControler = TextEditingController();
-  final valueControler = TextEditingController();
-
+class TransactionForm extends StatefulWidget {
   final void Function(String, double) onSubmit;
 
   TransactionForm(this.onSubmit);
+
+  @override
+  _TransactionFormState createState() => _TransactionFormState();
+}
+
+class _TransactionFormState extends State<TransactionForm> {
+  final titleControler = TextEditingController();
+
+  final valueControler = TextEditingController();
 
   _submitForm() {
     final title = titleControler.text;
@@ -16,7 +22,7 @@ class TransactionForm extends StatelessWidget {
       return;
     }
 
-    onSubmit(title, value);
+    widget.onSubmit(title, value);
   }
 
   @override
