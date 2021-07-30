21b607a4e145bd34ed9f9bbab6d9f7a9d655eec4
oscarbfs <oscar.borgesfs@gmail.com>
2021-07-30 15:17:13 -0300

    Modo Paisagem #03

--- lib/main.dart
+++ lib/main.dart
@@ -94,6 +94,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    bool isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
+
     final appBar = AppBar(
       title: Text(
         'Despesas Pessoais',
@@ -120,29 +123,30 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    "Exibir Gráfico",
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  Switch(
-                    value: _showChart,
-                    onChanged: (value) {
-                      setState(() {
-                        _showChart = value;
-                      });
-                    },
-                  ),
-                ],
-              ),
-              if (_showChart)
+              if (isLandscape)
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Exibir Gráfico",
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    Switch(
+                      value: _showChart,
+                      onChanged: (value) {
+                        setState(() {
+                          _showChart = value;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              if (_showChart || !isLandscape)
                 Container(
-                  height: availableHeight * 0.30,
+                  height: availableHeight * (isLandscape ? 0.7 : 0.30),
                   child: Chart(_recentTransactions),
                 ),
-              if (!_showChart)
+              if (!_showChart || !isLandscape)
                 Container(
                   height: availableHeight * 0.70,
                   child: TransactionList(_transactions, _removeTransaction),
