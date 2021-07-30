efb63d3b21a5642001b4943b26e83013cd16ab90
oscarbfs <oscar.borgesfs@gmail.com>
2021-07-30 11:01:57 -0300

    Usando LayoutBuilder

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class ChartBar extends StatelessWidget {
   final String label;
@@ -13,51 +14,60 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          height: 20,
-          child: FittedBox(
-            child: Text(
-              '${value.toStringAsFixed(2)}',
-              style: TextStyle(color: Colors.cyanAccent),
-            ),
-          ),
-        ),
-        SizedBox(height: 5),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            alignment: Alignment.bottomCenter,
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.cyanAccent,
-                    width: 1.0,
-                  ),
-                  borderRadius: BorderRadius.circular(5),
+    return LayoutBuilder(
+      builder: (ctx, constraints) {
+        return Column(
+          children: <Widget>[
+            Container(
+              height: constraints.maxHeight * 0.15,
+              child: FittedBox(
+                child: Text(
+                  '${value.toStringAsFixed(2)}',
+                  style: TextStyle(color: Colors.cyanAccent),
                 ),
               ),
-              FractionallySizedBox(
-                heightFactor: percentage,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.cyanAccent,
-                    borderRadius: BorderRadius.circular(5),
+            ),
+            SizedBox(height: constraints.maxHeight * 0.05),
+            Container(
+              height: constraints.maxHeight * 0.6,
+              width: 10,
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.cyanAccent,
+                        width: 1.0,
+                      ),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
                   ),
+                  FractionallySizedBox(
+                    heightFactor: percentage,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.cyanAccent,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: constraints.maxHeight * 0.05),
+            Container(
+              height: constraints.maxHeight * 0.15,
+              child: FittedBox(
+                child: Text(
+                  label.toUpperCase(),
+                  style: TextStyle(color: Colors.cyanAccent),
                 ),
-              )
-            ],
-          ),
-        ),
-        SizedBox(height: 5),
-        Text(
-          label.toUpperCase(),
-          style: TextStyle(color: Colors.cyanAccent),
-        )
-      ],
+              ),
+            )
+          ],
+        );
+      },
     );
   }
 }
